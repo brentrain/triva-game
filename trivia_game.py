b3c9b77d01351d66e31b0f017dfd5bb75d8d7dff
Brent Rainwater <brentvsmaximvs@gmail.com>
2020-02-08 14:24:36 -0500

    upload app file

---
+++ trivia_game.py
@@ -0,0 +1,11 @@
+#Program Name: Trivia Challenge
+#Author: Brent Rainwater
+#Date Started: 2/8/2020
+
+question_prompts = [
+    'What color are oranges? \n(a) red/green \n(b) purple \n(c) blue\n\n',
+    'What color are bananas? \n(a) red/green \n(b) yellow \n(c) blue\n\n',
+    'What color are bluebirds? \n(a) red/green \n(b) purple \n(c) blue\n\n'
+
+    
+]
